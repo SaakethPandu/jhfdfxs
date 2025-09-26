f1fde5de6e3bd68e5bef06ce0882fed72fdf5251
SaakethPandu <bhimanojulasaaketh@gmail.com>
2025-09-26 16:48:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const { Server } = require("socket.io");
+import { Server } from "socket.io";
+import http from "http";
 
+const server = http.createServer();
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 let players = {};
 let bullets = [];
@@ -8,72 +13,64 @@ function getRandomColor() {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-export default function handler(req, res) {
-  if (!res.socket.server.io) {
-    console.log("Setting up Socket.IO server...");
-    const io = new Server(res.socket.server);
+io.on("connection", (socket) => {
+  const playerId = socket.id;
+  players[playerId] = { x: 100, y: 100, color: getRandomColor(), health: 100, alive: true };
 
-    io.on("connection", socket => {
-      const playerId = socket.id;
-      players[playerId] = { x: 100, y: 100, color: getRandomColor(), health: 100, alive: true };
+  socket.emit("init", { id: playerId, players, bullets });
+  io.emit("update", { players, bullets });
 
-      socket.emit("init", { id: playerId, players, bullets });
-      io.emit("update", { players, bullets });
+  socket.on("move", (data) => {
+    if (players[playerId]?.alive) {
+      players[playerId].x = data.x;
+      players[playerId].y = data.y;
+    }
+  });
 
-      socket.on("move", (data) => {
-        if (players[playerId]?.alive) {
-          players[playerId].x = data.x;
-          players[playerId].y = data.y;
-        }
-      });
-
-      socket.on("shoot", (data) => {
-        bullets.push({
-          x: data.x,
-          y: data.y,
-          dx: data.dx,
-          dy: data.dy,
-          owner: playerId
-        });
-      });
-
-      socket.on("disconnect", () => {
-        delete players[playerId];
-        io.emit("update", { players, bullets });
-      });
+  socket.on("shoot", (data) => {
+    bullets.push({
+      x: data.x,
+      y: data.y,
+      dx: data.dx,
+      dy: data.dy,
+      owner: playerId
     });
+  });
 
-    // Bullet movement loop
-    setInterval(() => {
-      bullets.forEach((bullet, index) => {
-        bullet.x += bullet.dx;
-        bullet.y += bullet.dy;
+  socket.on("disconnect", () => {
+    delete players[playerId];
+    io.emit("update", { players, bullets });
+  });
+});
 
-        for (let id in players) {
-          if (id !== bullet.owner && players[id].alive) {
-            let p = players[id];
-            if (bullet.x > p.x && bullet.x < p.x + 50 && bullet.y > p.y && bullet.y < p.y + 50) {
-              p.health -= 10;
-              if (p.health <= 0) {
-                p.health = 0;
-                p.alive = false;
-              }
-              bullets.splice(index, 1);
-              break;
-            }
+setInterval(() => {
+  bullets.forEach((bullet, index) => {
+    bullet.x += bullet.dx;
+    bullet.y += bullet.dy;
+
+    for (let id in players) {
+      if (id !== bullet.owner && players[id].alive) {
+        let p = players[id];
+        if (bullet.x > p.x && bullet.x < p.x + 50 && bullet.y > p.y && bullet.y < p.y + 50) {
+          p.health -= 10;
+          if (p.health <= 0) {
+            p.health = 0;
+            p.alive = false;
           }
-        }
-
-        if (bullet.x < 0 || bullet.y < 0 || bullet.x > 2000 || bullet.y > 2000) {
           bullets.splice(index, 1);
+          break;
         }
-      });
+      }
+    }
 
-      io.emit("update", { players, bullets });
-    }, 30);
+    if (bullet.x < 0 || bullet.y < 0 || bullet.x > 2000 || bullet.y > 2000) {
+      bullets.splice(index, 1);
+    }
+  });
 
-    res.socket.server.io = io;
-  }
-  res.end();
-}
+  io.emit("update", { players, bullets });
+}, 30);
 
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
